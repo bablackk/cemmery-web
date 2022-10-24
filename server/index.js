33b3511ec0f8a3e17e6867699d573c39c1430a74
lequang03 <lethanhquang147@gmail.com>
2022-10-24 12:34:58 +0700

    db user and handle login, register

--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ const cors = require("cors");
 const { mongoose } = require("mongoose");
 const { PORT, URI_MONGODB } = require("./config/index.config");
 const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth.route");
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
-// connect to db
+// connect to DB
 mongoose.connect(URI_MONGODB, async () => {
   await console.log("DB connected");
 });
+
+// Route
+app.use("/v1/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
