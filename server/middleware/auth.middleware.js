035ec7529057d7c54678e740d311394da64c4c2b
lequang03 <lethanhquang147@gmail.com>
2022-11-24 19:18:50 +0700

    add feature login

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -3,6 +3,9 @@ const User = require("../models/user.model");
 module.exports.requireAuth = async (req, res, next) => {
   const cookieUser = req.signedCookies.user_id;
   const user = await User.findById(cookieUser);
+  if (!user) {
+    res.redirect("/login");
+  }
   res.locals.user = user;
   next();
 };
