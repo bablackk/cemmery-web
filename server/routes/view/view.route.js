4e839badb4ca604b0bfa77456f923da7ec5f6d50
lequang03 <lethanhquang147@gmail.com>
2022-12-01 16:48:58 +0700

    final #1

--- server/routes/view/view.route.js
+++ server/routes/view/view.route.js
@@ -39,7 +39,7 @@ router
   .post(viewController.registerHandle);
 router.get("/logout", viewController.logoutHandle);
 router.get("/cart", authMiddleware.requireAuth, viewController.cart);
-router.get("/admin", viewController.adminPage);
+router.get("/admin", authMiddleware.requireAuth, viewController.adminPage);
 router
   .route("/payment")
   .get(authMiddleware.requireAuth, viewController.checkout)
@@ -49,4 +49,24 @@ router.get("/profile", authMiddleware.requireAuth, viewController.profile);
 router.get("/search:productName", viewController.search);
 router.get("/payment/thanks", viewController.thanks);
 
+//ADMIN PANEL
+
+router.get("/admin/products", viewController.renderProduct);
+router
+  .route("/admin/product/add")
+  .get(viewController.renderPageAddProduct)
+  .post(viewController.AdminAddProducts);
+
+router.get(
+  "/admin/profile/:id",
+  authMiddleware.requireAuth,
+  viewController.AdminProfile
+);
+
+router.get(
+  "/admin/orders",
+  authMiddleware.requireAuth,
+  viewController.AdminOrder
+);
+router.get("/admin/users", authMiddleware.requireAuth, viewController.AllUser);
 module.exports = router;
